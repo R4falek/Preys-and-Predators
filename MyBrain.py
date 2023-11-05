c8d5ca512fc5307138e7fa70f6d0e303026910af
rmamak <rmamak@core-logic.com>
2023-11-05 15:29:20 +0100

    update

---
+++ MyBrain.py
@@ -0,0 +1,9 @@
+import numpy as np
+class MyBrain():
+    def __init__(self, input_layer):
+        self.input_layer_weights = np.zeros((input_layer + 1, 3))
+        self.output_layer_weights = np.zeros(())
+
+    # def forward(self, input_vector):
+
+
